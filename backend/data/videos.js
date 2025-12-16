8420b36c6df21624ca1bdedc519675b347d018b4
bbelguith <bbelguith@addelink.com>
2025-12-16 14:21:31 +0100

    2Kquality

--- backend/data/videos.js
+++ backend/data/videos.js
@@ -1,22 +1,22 @@
 export const videos = [
   {
-    src: "https://res.cloudinary.com/dueoeevmz/video/upload/v1765661681/2_fqtpzq.mp4",
+    src: "https://res.cloudinary.com/dueoeevmz/video/upload/f_auto,q_auto,vc_h265,w_2160/v1765661681/2_fqtpzq.mp4",
     title: "House AB"
   },
   {
-    src: "https://res.cloudinary.com/dueoeevmz/video/upload/v1765661683/3_ryua0p.mp4",
+    src: "https://res.cloudinary.com/dueoeevmz/video/upload/f_auto,q_auto,vc_h265,w_2160/v1765661683/3_ryua0p.mp4",
     title: "House BC"
   },
   {
-    src: "https://res.cloudinary.com/dueoeevmz/video/upload/v1765661656/4_ja22t2.mp4",
+    src: "https://res.cloudinary.com/dueoeevmz/video/upload/f_auto,q_auto,vc_h265,w_2160/v1765661656/4_ja22t2.mp4",
     title: "House CA"
   },
   {
-    src: "https://res.cloudinary.com/dueoeevmz/video/upload/v1765661645/5_qe4aal.mp4",
+    src: "https://res.cloudinary.com/dueoeevmz/video/upload/f_auto,q_auto,vc_h265,w_2160/v1765661645/5_qe4aal.mp4",
     title: "House CA"
   },
   {
-    src: "https://res.cloudinary.com/dueoeevmz/video/upload/v1765661528/6_klnaug.mp4",
+    src: "https://res.cloudinary.com/dueoeevmz/video/upload/f_auto,q_auto,vc_h265,w_2160/v1765661528/6_klnaug.mp4",
     title: "House CA"
   },
 
